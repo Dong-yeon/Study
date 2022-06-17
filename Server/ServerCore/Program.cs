72dad6989a9d249542cfd0246e775852152395a7
Dong-yeon <84108535+Dong-yeon@users.noreply.github.com>
2022-06-17 17:43:22 +0900

    컴파일러 최적화 Release 모드에서의 컴파일러 최적화 생각

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -6,54 +6,35 @@ namespace ServerCore
 {
     class Program
     {
+        volatile static bool _stop = false;
 
-        static void MainThread(object state)
+        static void ThreadMain()
         {
+            Console.WriteLine("쓰레드 시작!");
 
-            for (int i = 0; i < 5; i++)
-                Console.WriteLine("Hello Thread!");
+            while (_stop == false)
+            {
+                // 누군가가 stop 신호를 해주기를 기다린다.
+            }
+
+            Console.WriteLine("쓰레드 종료!");
         }
-
 
         static void Main(string[] args)
         {
+            Task t = new Task(ThreadMain);
+            t.Start();
 
-            // 쓰레드 풀 구현
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMinThreads(5, 5);
+            Thread.Sleep(1000);
 
-            // Task 구현
+            _stop = true;
 
-            for (int i = 0; i < 5; i++)
-            {
-                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
-                t.Start();
-            }
-
-            //for (int i=0; i < 4; i++)
-            //{
-            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
-            //}
-
-            ThreadPool.QueueUserWorkItem(MainThread);
-
-            // 멀티 쓰레드 구현
-            //for (int i = 0; i < 1000; i++)
-            //{
-            //    Thread t = new Thread(MainThread);
-            //    //t.Name = "Test Thread";
-            //    t.IsBackground = true;
-            //    t.Start();
-            //}
-            //Console.WriteLine("Waiting for Thread");
-            //t.Join();
-
-            //Console.WriteLine("Hello world");
-
-            while (true)
-            {
-
-            }
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
+            
+            t.Wait();
+            
+            Console.WriteLine("종료 성공");
         }
 
     }
