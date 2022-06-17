da7b489d1de74e03632bde0b30c9ec351a3d1460
Dong-yeon <84108535+Dong-yeon@users.noreply.github.com>
2022-06-17 17:28:33 +0900

    쓰레드 생성

---
+++ Server/ServerCore/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+
+        static void MainThread(object state)
+        {
+
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("Hello Thread!");
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            // 쓰레드 풀 구현
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMinThreads(5, 5);
+
+            // Task 구현
+
+            for (int i = 0; i < 5; i++)
+            {
+                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
+                t.Start();
+            }
+
+            //for (int i=0; i < 4; i++)
+            //{
+            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+            //}
+
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            // 멀티 쓰레드 구현
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    Thread t = new Thread(MainThread);
+            //    //t.Name = "Test Thread";
+            //    t.IsBackground = true;
+            //    t.Start();
+            //}
+            //Console.WriteLine("Waiting for Thread");
+            //t.Join();
+
+            //Console.WriteLine("Hello world");
+
+            while (true)
+            {
+
+            }
+        }
+
+    }
+}
