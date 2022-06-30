3986ab814f4edf168ffcc1f5440c9a0e0dd7625c
Dong-yeon <84108535+Dong-yeon@users.noreply.github.com>
2022-06-30 10:28:15 +0900

    DeadLock

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,6 +4,54 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class FastLock
+    {
+        int id;
+    }
+
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+       
+        public static void Test()
+        {
+            lock(_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+    }
+
     class Program
     {
         static int number = 0;
@@ -11,27 +59,9 @@ namespace ServerCore
 
         static void Thread_1()
         {
-            // atomic = 원자성
-
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < 100000; i++)
             {
-                // 상호배제 Mutual Exclusive
-                lock(_obj)
-                {
-                    number++;
-                }
-
-/*                try
-                {
-                    Monitor.Enter(_obj); // 문을 잠그는 행위
-                    number++;
-
-                    return;
-                }
-                finally
-                {
-                    Monitor.Exit(_obj);
-                }*/
+                SessionManager.Test();
             }
         }
 
@@ -39,12 +69,9 @@ namespace ServerCore
 
         static void Thread_2()
         {
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < 100000; i++)
             {
-                lock (_obj)
-                {
-                    number--;
-                }
+                UserManager.Test();
             }
         }
 
