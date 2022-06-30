8f29b699e52b6728dc3b9943b184fb1ffbe97169
Dong-yeon <84108535+Dong-yeon@users.noreply.github.com>
2022-06-30 15:00:43 +0900

    ReaderWriterLock 구현 연습

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,55 +1,42 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServerCore
 {
     class Program
     {
-        // 1. 근성
-        // 2. 양보
-        // 3. 갑질
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
 
-        // 상호배제
-        // Monitor
-        static object _lock = new object();
-        static SpinLock _lock2 = new SpinLock();
-        // RWLock RederWriteLock
-        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
-
-        // 직접 만든다.
-
-        // [ ] [ ] [ ] [ ] [ ] 
-        class Reward
-        {
-
-        }
-
-        // 99.999999999% 0.0000000001%
-        static Reward GetRewardById(int id)
-        {
-            _lock3.EnterReadLock();
-
-            _lock3.ExitReadLock();
-
-            return null;
-        }
-
-        // 0.000001%
-        void AddReward(Reward reward)
-        {
-            _lock3.EnterReadLock();
-
-            _lock3.ExitReadLock();
-
-        }
 
         static void Main(string[] args)
         {
-            lock (_lock)
+            Task t1 = new Task(delegate ()
             {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnLock();
+                }
+            });
 
-            }
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnLock();
+                }
+            });
+
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(count);
         }
     }
 }
